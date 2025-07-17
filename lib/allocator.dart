c7e6111a144b8515cdb844200f72ea21b1a67314
Sami <su0355486@gmail.com>
2025-07-17 14:21:24 +0500

    create locator file

---
+++ lib/allocator.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+import 'package:jetpow/core/services/local_storage_service.dart';
+
+
+GetIt locator = GetIt.instance;
+
+void setupLocator()async{
+
+  locator.registerSingleton(LocalStorageService());
+
+
+}
